862dda96df1e2c2882eef85448285f4249e2f780
dcubides <davidcubides.90@gmail.com>
2019-03-10 16:47:14 -0500

    show splash screen

--- AppGuia4/src/app/app.component.ts
+++ AppGuia4/src/app/app.component.ts
@@ -3,24 +3,36 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+//import { timer} from 'rxjs/observable/timer';
+
+import { timer } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    this.splashScreen.show();
+
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.splashScreen.show();
+
+
+      timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
